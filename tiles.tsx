f0f16c5e4a0682f953b67389bd515015eb611112
LineChangerr <dim.shvalev@gmail.com>
2024-01-17 04:52:09 +0300

    появление карты в питоне тз и requirements.txt

--- tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="image_2023-12-29_17-21-49" tilewidth="32" tileheight="32" tilecount="12480" columns="312">
- <image source="image_2023-12-29_17-21-49.png" width="9985" height="1281"/>
-</tileset>
